b0e56186ff8ae89d8bf6a0a51df2829bc3bcb12f
Dhik01 <andikamuhammad48@gmail.com>
2025-05-14 21:01:08 +0700

    [update] add seeder for carousel data, cause static data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Carousel;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Carousel::insert([
+            [
+                'slug' => 'CRL0',
+                'title' => 'Apotek',
+                'subtitle' => 'Peduli',
+                'description' => 'Selamat datang di Apotek Terpercaya. Kami menyediakan obat berkualitas dan layanan kesehatan profesional.',
+                'imageCarousel' => 'img/slider-apotek-img.png',
+            ],
+            [
+                'slug' => 'CRL1',
+                'title' => 'Kesehatan',
+                'subtitle' => 'Utama',
+                'description' => 'Kami menyediakan vitamin dan suplemen untuk menjaga kesehatan Anda.',
+                'imageCarousel' => 'img/slider-apotek-2-img.png',
+            ],
+            [
+                'slug' => 'CRL2',
+                'title' => 'Obat',
+                'subtitle' => 'Berkualitas',
+                'description' => 'Berbagai obat terpercaya dan aman tersedia di apotek kami.',
+                'imageCarousel' => 'img/slider-apotek-3-img.png',
+            ]
         ]);
     }
 }
